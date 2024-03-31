8a0c1488d5b972f7a72a207b82e7c702e1dddc4e
Rajkumar Natarajan <raj.cs.psg@gmail.com>
2024-03-31 12:54:23 -0700

    make openapi work

---
+++ test/ring/openapi/openapi3_test.clj
@@ -0,0 +1,22 @@
+(ns ring.openapi.openapi3-test
+  (:require [clojure.test :refer :all]
+            [ring.openapi.openapi3 :refer :all]
+            [schema.core :as s]
+            [ring.swagger.swagger2 :as swagger2]
+            [ring.swagger.swagger2-full-schema :as full-schema]
+            [ring.swagger.json-schema :as rsjs]
+            [ring.swagger.extension :as extension]
+            [ring.swagger.validator :as validator]
+            [linked.core :as linked]
+            [ring.util.http-status :as status]
+            [midje.sweet :refer :all])
+  (:import [java.util Date UUID]
+           [java.util.regex Pattern]
+           [org.joda.time DateTime LocalDate LocalTime]))
+(s/defschema Tag
+  {(s/optional-key :id) (rsjs/field s/Int {:description "Unique identifier for the tag"})
+   (s/optional-key :name) (rsjs/field s/Str {:description "Friendly name for the tag"})})
+(def swagger-response {:paths {"/api" {:post { :requestBody {:content {"application/json" {:foo s/Str}}} :responses {200 {:description "ok" :schema Tag}}}}}})
+
+#_(deftest swagger-response-test
+  (clojure.pprint/pprint (openapi-json swagger-response)))
